7dd02b3be8f569ef6647f20e40e2aa55ef6a8f4d
naoto0822 <n.h.in.m.h@gmail.com>
2017-03-23 00:17:55 +0900

    install command line tools

--- etc/init.sh
+++ etc/init.sh
@@ -7,6 +7,14 @@ echo "-----------------------------------------"
 # now mac OS
 echo "[INFO] For macOS script"
 
+# Command Line Tools
+if [ ! `which xcode-select` ]; then
+  xcode-select --install
+  echo "[INFO] installed successfully Command Line Tools !"
+else
+  echo "[INFO] already installed Command Line Tools"
+fi
+
 # homebrew
 if [ ! `which brew` ]; then
 	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
