fe63d708de4f8d20a177b08d567416b4ab5f171c
naoto0822 <n.h.in.m.h@gmail.com>
2017-04-15 00:15:35 +0900

    install vim, zsh in linux

--- etc/linux.sh
+++ etc/linux.sh
@@ -6,3 +6,35 @@ CURRENT_PATH=$(cd $(dirname $0) && pwd)
 
 info_log "[INFO] exec linux.sh"
 
+# for CentOS
+
+# Vim
+yum -y install make
+yum -y install gcc
+yum -y install mercurial
+yum -y install ncurses-devel
+yum -y install lua-devel
+yum -y install ruby-devel
+yum -y install perl-ExtUtils-Embed
+
+wget ftp://ftp.vim.org/pub/vim/unix/vim-8.0.tar.bz2
+tar xvf vim-8.0.tar.bz2
+cd vim80
+
+./configure \
+  --enable-multibyte \
+  --with-features=huge \
+  --enable-luainterp \
+  --enable-perlinterp \
+  --enable-pythoninterp \
+  --with-python-config-dir=/usr/lib64/python2.6/config \
+  --enable-rubyinterp \
+  --with-ruby-command=/usr/bin/ruby \
+
+make
+make install
+
+# Zsh
+yum -y install zsh
+chsh -s /bin/zsh
+
