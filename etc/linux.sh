0693331953b4ed5981149fd43ea4252ac0f7482d
naoto0822 <n.h.in.m.h@gmail.com>
2017-11-16 00:11:38 +0900

    fix vim building

--- etc/linux.sh
+++ etc/linux.sh
@@ -15,9 +15,17 @@ sudo yum -y install mercurial
 sudo yum -y install ncurses-devel
 sudo yum -y install lua
 sudo yum -y install lua-devel
+sudo yum -y install luajit
+sudo yum -y install luajit-devel
 sudo yum -y install ruby-devel
+sudo yum -y install perl
+sudo yum -y install perl-devel
+sudo yum -y install perl-ExtUtils-ParseXS
+sudo yum -y install perl-ExtUtils-XSpp
+sudo yum -y install perl-ExtUtils-CBuilder
 sudo yum -y install perl-ExtUtils-Embed
-sudo yum -y install python-devel
+sudo yum -y install tcl-devel
+sudo yum -y install ctags
 sudo yum -y install zsh
 sudo yum -y install openssl-devel
 sudo yum -y install readline-devel
@@ -36,22 +44,21 @@ cd vim/
 git pull
 sudo make distclean
 
+WHICH_RUBY=$(which ruby)
+
 sudo ./configure \
-  --prefix=/usr/local \
+  --prefix=/usr \
   --with-features=huge \
   --enable-multibyte \
   --enable-cscope \
   --enable-perlinterp \
-  --enable-pythoninterp=dynamic \
-  --with-python-config-dir=/usr/lib64/python2.6/config \
-  --enable-rubyinterp=dynamic \
-  --with-ruby-command=~/.rbenv/shims/ruby \
-  --enable-luainterp=yes \
-  --with-lua-prefix=/usr/local \
-  --enable-fail-if-missing \
+  --enable-rubyinterp \
+  --with-ruby-command=$WHICH_RUBY \
+  --enable-luainterp \
 
 sudo make
 sudo make install
+hash -r
 
 cd $HOME/dotfiles
 
