9049751d17ffe95cc75b5657ca793c61c8e97ff9
naoto0822 <n.h.in.m.h@gmail.com>
2017-12-22 02:28:31 +0900

    fix

--- src/common.sh
+++ src/common.sh
@@ -4,10 +4,10 @@ CURRENT_PATH=$(cd $(dirname $0) && pwd)
 
 . "$CURRENT_PATH"/logger.sh
 
-info_log "exec common.sh"
+info_log "---> exec common.sh"
 
 # vim-plug
-info_log "install vim-plug"
+info_log "---> install vim-plug"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
