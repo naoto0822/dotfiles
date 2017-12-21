8a8485ee5d5a156f0d84cec9a4efa1822169a47f
naoto0822 <n.h.in.m.h@gmail.com>
2017-12-22 02:50:12 +0900

    add yeah

---
+++ src/say_yeah.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+CURRENT_PATH=$(cd $(dirname $0) && pwd)
+. "$CURRENT_PATH"/logger.sh
+
+yeah='
+
+ __   __  _____      _      _   _
+ \ \ / / | ____|    / \    | | | |
+  \ V /  |  _|     / _ \   | |_| |
+   | |   | |___   / ___ \  |  _  |
+   |_|   |_____| /_/   \_\ |_| |_|
+
+'
+
+info_log "$yeah"
